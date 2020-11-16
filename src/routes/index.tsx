2103b66409c49cac81354ed521fbc75c133999bd
brunoomsantos <Bb030996>
2020-11-16 15:00:20 -0300

    Alguma coisa ae

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,16 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import SingIn from "../pages/Singin";
+import SingUp from "../pages/SingUp";
+
 const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+    <Auth.Navigator>
+        <Auth.Screen name="SingIn" component={SingIn} />
+        <Auth.Screen name="SingUp" component={SingUp} />
+    </Auth.Navigator>
+);
+
+export default AuthRoutes;
